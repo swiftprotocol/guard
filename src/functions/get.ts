494a7c0e15b986a4425102091c6ce19edf338044
Josef Leventon <josef.leventon@gmail.com>
2023-05-31 17:40:19 -0400

    v0.4.1

--- src/functions/get.ts
+++ src/functions/get.ts
@@ -5,7 +5,7 @@ import { signAuthorizationMessage } from "../helpers/sign.js";
 export default async function get(key: string, namespace?: string) {
   const account = this.account || (await getAccount(this.chainId));
 
-  const msg = await signAuthorizationMessage.call(this);
+  const msg = this.sig || (await signAuthorizationMessage.call(this));
 
   const result = await axios
     .post(this.api + `/retrieve/${account.address}/${key}`, {
